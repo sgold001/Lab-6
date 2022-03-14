ef82924e533b9309625be2ad588539736e29a45a
sgold001 <101606001+sgold001@users.noreply.github.com>
2022-03-14 19:40:49 -0400

    Lab 6 Add Numbers

---
+++ Lab6.java
@@ -0,0 +1,19 @@
+
+import java.util.Scanner;
+public class Lab6 {
+    public static void main(String[]args){
+
+
+        Scanner scan= new Scanner(System.in);
+ 
+        System.out.print("Enter the first number: ");
+        int firstNum= scan.nextInt();
+
+        System.out.print("Enter the second  number: ");
+        int secondNum= scan.nextInt();
+
+        System.out.print( "The sum is: " +(firstNum + secondNum));
+    }
+
+
+}
